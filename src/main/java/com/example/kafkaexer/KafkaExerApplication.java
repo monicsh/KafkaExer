8276acd4ed360d47f76ebff7130fb2b377691705
Monika Sharma <monic.sh@gmail.com>
2022-10-07 19:11:47 -0700

    Kafka Spring boot project

---
+++ src/main/java/com/example/kafkaexer/KafkaExerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaexer/KafkaExerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
