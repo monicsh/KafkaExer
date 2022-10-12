f636240ebcdb96339f413dc6aa8557a78e0fc2fa
Monika Sharma <monic.sh@gmail.com>
2022-10-12 15:27:16 -0700

    configuration

---
+++ src/main/java/com/example/kafkaexer/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.example.kafkaexer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic monicodeTopic() {
+        return TopicBuilder.name("monicode")
+                .build();
+    }
+}
